7392426dcdbfe97b771cd37631fb5c93d5b23829
Rachel Wilkins <rachelwilkins417@gmail.com>
2025-04-04 22:01:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Create a project named homework-eight <br>
+Create a navigation bar that has four links<br>
+Add content to the page when you click on a link by using jQuery<br>
+Use only one page and load all the content onto that page<br>
+All pages must have full content. Images and text on every page and professionally styled<br>
+Upload your project to Github and add your Web4 link to the README file. Then, submit the Github link to this assignment<br>
+
+Web 4 link to homework-eight:
---
+++ app/app.js
@@ -0,0 +1,121 @@
+var homePage = `<div class="home-content">
+        <div class="hero"></div>
+        <div class="home-middle">
+          <h2 class="middle-header">Welcome to the Home Page!</h2>
+          <div class="middle-text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque atque iusto ut magnam! Cumque ad blanditiis esse voluptas quibusdam sapiente enim consectetur quas maiores, architecto corrupti officiis libero, dolore quo.</p>
+            <p>Beatae delectus fugiat quis, ipsa vel eaque laborum, reiciendis, quam ducimus eius quasi recusandae atque? Est laboriosam, autem repellat minima quisquam ipsa quos, ex sequi eum architecto adipisci distinctio tempore!</p>
+          </div>
+          <div class="home-middle-pictures">
+            <img src="images/home-gallery1.jpg" alt="Worship1">
+            <img src="images/home-gallery2.jpg" alt="Sermon">
+            <img src="images/home-gallery3.jpg" alt="Worship2">
+          </div>
+        </div>
+        <div class="home-bottom">
+          <div class="home-left">
+            <h2>Join Us For Easter!!</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id doloremque libero consectetur laboriosam, corporis veritatis labore. Qui amet ea at adipisci, tempore ullam alias, velit nihil reprehenderit inventore natus, quisquam numquam molestiae dolore culpa nam sapiente eveniet tenetur repudiandae ut suscipit. Quos voluptate atque possimus est deleniti dolorem corporis impedit?</p>
+          </div>
+          <div class="home-right"></div>
+        </div>
+      </div>`;
+var aboutPage = `<div class="about-content">
+            <h2 class="about-header">About Us!</h2>
+            <div class="about-profiles">
+                <img src="images/bri.jpg" alt="Bri">
+                <img src="images/john.jpg" alt="John">
+                <img src="images/matt2.jpg" alt="Matt">
+            </div>
+            <div class="about-paragraphs">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo reiciendis magnam molestiae fuga rem commodi, placeat quae aliquid ipsa eligendi praesentium quaerat odit facilis. Quaerat nisi at aliquid ipsa natus placeat earum deleniti, amet tempora harum eligendi nulla voluptas tenetur reiciendis doloribus odio dignissimos sed? Voluptatum, vero libero suscipit blanditiis, qui illum maxime quasi, dolorem neque ipsum odit repellendus eum. Vero consequuntur sunt, dolorem doloribus optio velit qui dicta et a aliquid expedita id laborum, tempore, tenetur veniam! Possimus quasi perspiciatis commodi quis praesentium sapiente nihil fuga quaerat mollitia error. Mollitia omnis harum doloremque inventore doloribus, facilis delectus asperiores voluptatum.</p>
+                <p>Debitis explicabo enim, dicta rerum maiores necessitatibus quod nobis eos ullam ad praesentium quisquam amet quia error delectus dolore voluptatem voluptate! Officia atque similique distinctio, maxime quae, vero placeat voluptas architecto voluptate ad, ipsam consequuntur! Velit, repellendus at. Veritatis architecto iusto, quaerat perferendis explicabo vero cumque at tempore nulla consequatur? Repellendus quas est, odit repellat provident asperiores, molestias distinctio cupiditate, eos nihil expedita. Odio delectus ab alias nulla modi reprehenderit eveniet, ad atque optio, suscipit, minima obcaecati qui exercitationem? Est, porro. Odit dolore culpa quidem error, ullam quisquam quo maxime architecto quos? Beatae exercitationem iusto iure, harum non delectus placeat?</p>
+                <p>Nesciunt numquam a quasi illum excepturi. Rem, unde amet, aspernatur quod explicabo, eveniet porro quam debitis possimus accusantium eligendi distinctio error provident asperiores fugiat suscipit molestiae? Nesciunt laudantium in repellendus quam minus voluptas, commodi amet explicabo distinctio aspernatur sunt voluptates eum nam aliquam error sequi perferendis tempore. Neque magni odio deleniti dicta rem necessitatibus cum dolorum exercitationem quibusdam, illum reprehenderit unde ut, nemo sed accusantium magnam minus sequi alias error sit nam maxime assumenda mollitia! Necessitatibus similique aperiam, quasi labore quam consequuntur id saepe nobis facilis molestias asperiores harum ducimus praesentium deleniti eius earum suscipit voluptates tenetur rem error hic.</p>
+            </div>
+        </div>`;
+var easterPage = ` <div class="easter-content">
+            <div class="easter-hero">
+                <img src="images/easterHeader.jpg" alt="Easter">
+            </div>
+            <div class="easter-middle-content">
+                <div class="easter-left"><h1>You're invited to Easter with CP!</h1>
+                <p class="easter-sl"><strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, iure.</strong></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ex tenetur inventore laudantium laboriosam nisi!
+                    Atque ratione rerum eveniet commodi cupiditate repellendus natus, repellat soluta voluptatem esse! Whoever is reading this you should definitely come! Tempora
+                    distinctio ipsum optio dolore quae quidem, explicabo qui modi nemo quia, facilis amet, quasi error. Commodi earum
+                    aspernatur cupiditate magni fugit ut.</p>
+                <button><strong>SERVICE TIMES & LOCATIONS</strong></button>
+            </div>
+            <div class="easter-right">
+                <img src="images/easterinvite.png" alt="easter invite">
+            </div>
+            </div>
+            <div class="easter-gallery">
+                <div class="easter-picture-row">
+                    <div class="easter-picture-holder">
+                        <img src="images/easter1.jpg" alt="Easter">
+                    </div>
+                    <div class="easter-picture-holder">
+                        <img src="images/easter2.jpg" alt="Easter">
+                    </div>
+                    <div class="easter-picture-holder">
+                        <img src="images/easter3.jpg" alt="Easter">
+                    </div>
+                </div>
+                <div class="easter-picture-row">
+                    <div class="easter-picture-holder">
+                        <img src="images/easter4.jpg" alt="Easter">
+                    </div>
+                    <div class="easter-picture-holder">
+                        <img src="images/easter5.jpg" alt="Easter">
+                    </div>
+                    <div class="easter-picture-holder">
+                        <img src="images/easter6.jpg" alt="Easter">
+                    </div>
+                </div>
+            </div>
+        </div>`;
+var contactPage = `<div class="contact-content">
+            <h1 class="contact-header">Contact Us!</h1>  
+            <div class="contact-holder">
+                <div class="contact-left"></div>
+                <div class="contact-right">
+                    <div class="contact-form">
+                        <input text placeholder="Your name..."> 
+                        <input text placeholder="Phone Number..."> 
+                        <input text placeholder="Email Address...">
+                        <form>
+                            <textarea>Message...</textarea>
+                        </form>
+                    </div>
+                    <div class="contact-button">
+                        <button>SEND MESSAGE</button>
+                    </div>
+                </div>
+            </div>        
+        </div>`;
+
+function fillPageStuff (pageName) {
+    let pageContent = pageName + "Page";
+    $("#app").html(eval(pageContent));
+}
+
+function initApp() {
+    fillPageStuff("home");
+
+    $("nav a").on("click", (e) => {
+        e.preventDefault();
+        // let btnID = $(e.target).attr("id");
+        let btnID = e.currentTarget.id;
+
+        if(btnID == "help") {
+            helpMe();
+        } else {
+            fillPageStuff(btnID);
+        }
+    });
+}
+
+$(document).ready(() => {
+    initApp();
+});
